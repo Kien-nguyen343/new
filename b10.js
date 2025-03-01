dc270ad40a4ec679b7e050c2e1ce4cf0e5861740
Kien-nguyen343 <lucifernguyen10@gmail.com>
2025-03-01 14:53:07 +0700

    r

--- b10.js
+++ b10.js
@@ -1,18 +1,14 @@
 const API_KEY = '4269652c5e1b6a66a34f9d3d16ecc098';
 const DEFAULT_VALUE = '--';
-const searchInput = document.getElementById("search-input");
-const cityName = document.querySelector(".city-name");
+const cityName = document.querySelector(".city-name").textContent;
 const weatherState = document.querySelector(".weather-state");
 const weatherIcon = document.querySelector(".weather-icon");
 const temperature = document.querySelector(".temperature");
 
-searchInput.addEventListener("change", (event) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=vi&units=metric`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&lang=vi&units=metric`)
         .then(response => response.json())
         .then((data) => {
             cityName.innerHTML = data.name || DEFAULT_VALUE;
             weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
             weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` || DEFAULT_VALUE;
-            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;          
-        })
-})
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;     })
